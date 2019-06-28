89ca0fd85b21d1fceb1f14d890239586f777ca4c
Jan <jan.m.muller@icloud.com>
2019-06-28 16:01:56 +0200

    various updates, Linux should now be supported

--- personalScripts/docker-cleanup.sh
+++ personalScripts/docker-cleanup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-docker rmi $(docker images -q -f dangling=true)
+docker image prune -a -f --filter "until=240h"
+docker container prune -f --filter "until=240h"
+
+docker rmi $(docker images -q -f dangling=true) || /usr/bin/true
 docker volume rm $(docker volume ls -qf dangling=true)
